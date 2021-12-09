2b45372226925ff4e573093bf4aa797450a1f728
Cristiano Eckel <51169878+cris1eckel@users.noreply.github.com>
2021-12-09 15:45:51 -0300

    ADD

--- unisc/src/ClientHandler.java
+++ unisc/src/ClientHandler.java
@@ -1,11 +1,14 @@
 import java.io.*;
 import java.net.Socket;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -31,27 +34,6 @@ class ClientHandler extends Thread {
         String toreturn;
         while (true) {
             try {
-
-                // Ask user what he wants
-                dos.writeUTF("Adicione uma nova linha ao arquivo: (escreva Exit, para encerrar a sessão) ");
-
-                // receive the answer from client
-                received = dis.readUTF();
-
-                if (received.equals("Exit")) {
-                    System.out.println("Client " + this.s + " sends exit...");
-                    System.out.println("Closing this connection.");
-                    this.s.close();
-                    System.out.println("Connection closed");
-                    break;
-                }
-
-                // creating Date object
-                Date date = new Date();
-
-                // write on output stream based on the
-                // answer from the client
-
                 try {
 
                     Path path = Paths.get("C:\\Users\\cristiano.eckel\\Documents\\GitHub\\ppd\\unisc\\documento.txt");
@@ -64,50 +46,40 @@ class ClientHandler extends Thread {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+                // Ask user what he wants
+                dos.writeUTF("Adicione uma nova linha ao arquivo: (escreva Exit, para encerrar a sessão | Spam para inserir dados aleatórios ao arquivo) ");
+
+                // receive the answer from client
+                received = dis.readUTF();
+
+                if (received.equals("Exit")) {
+                    System.out.println("Client " + this.s + " sends exit...");
+                    System.out.println("Closing this connection.");
+                    this.s.close();
+                    System.out.println("Connection closed");
+                    break;
+                }
+
+                if (received.equals("Spam")) {
+                    try (FileWriter f = new FileWriter("C:\\Users\\cristiano.eckel\\Documents\\GitHub\\ppd\\unisc\\documento.txt", true);
+                         BufferedWriter b = new BufferedWriter(f);
+                         PrintWriter p = new PrintWriter(b)) {
+                       for (int x=0;  x < 30; x++){
+                           p.println(randomString());
+                        }
+
+                    }
+                }
+
+                // write on output stream based on the
+                // answer from the client
+
                 try (FileWriter f = new FileWriter("C:\\Users\\cristiano.eckel\\Documents\\GitHub\\ppd\\unisc\\documento.txt", true);
                      BufferedWriter b = new BufferedWriter(f);
                      PrintWriter p = new PrintWriter(b);) {
                     p.println(received);
                 }
 
-//                switch (received) {
-//
-//                    case "Digitar" :
-//                        dos.writeUTF("Digite seu texto: ");
-//                        try (FileWriter f = new FileWriter("C:\\Users\\cristiano.eckel\\Documents\\GitHub\\ppd\\unisc\\documento.txt", true);
-//                             BufferedWriter b = new BufferedWriter(f);
-//                             PrintWriter p = new PrintWriter(b);) {
-//                            p.println(received);
-//                            //ABRIR ARQUIVO
-//                            try {
-//                                Path path = Paths.get("C:\\Users\\cristiano.eckel\\Documents\\GitHub\\ppd\\unisc\\documento.txt");
-//                                Stream<String> lines = Files.lines(path);
-//
-//                                String content = lines.collect(Collectors.joining(System.lineSeparator()));
-//                                toreturn = content;
-//                                dos.writeUTF(toreturn);
-//                                lines.close();
-//                            } catch (IOException e) {
-//                                e.printStackTrace();
-//                            }
-//                        }
-//                        break;
-//
-//                    case "Editar" :
-//                        toreturn = fortime.format(date);
-//                        dos.writeUTF(toreturn);
-//                        break;
-//
-//                    case "Excluir" :
-//                        toreturn = fortime.format(date);
-//                        dos.writeUTF(toreturn);
-//                        break;
-//
-//
-//                    default:
-//                        dos.writeUTF("Invalid input");
-//                        break;
-//                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -121,5 +93,12 @@ class ClientHandler extends Thread {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+    }
+    public String randomString() {
+        byte[] array = new byte[7]; // length is bounded by 7
+        new Random().nextBytes(array);
+        return new String(array, StandardCharsets.UTF_8);
+
     }
 }
